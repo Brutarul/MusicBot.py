b6346e338192b94ce867c763e6693c83f743db40
Baker <silviurosu9@gmail.com>
2024-07-26 13:11:47 +0200

    Update Bot.py

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 import config
 from discord.ext import commands
+from Cogs.music_cog import MusicCog
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,7 +14,7 @@ async def on_ready():
     await bot.tree.sync()
 
 async def main():
-    await bot.load_extension("music_cog")
+    await bot.load_extension(MusicCog)
     async with bot:
         await bot.run(config.Token)
 
