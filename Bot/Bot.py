1d75b734e901645a1c228aed4d248a0f84525046
Baker <silviurosu9@gmail.com>
2024-07-26 13:03:52 +0200

    Update Bot.py

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
+import config
 
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    await bot.tree.sync()
+
+async def setup():
+    await bot.load_extension("music_cog")
+
+bot.loop.create_task(setup())
+bot.run(config.TOKEN)
